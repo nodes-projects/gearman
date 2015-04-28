3a4cb7df2ed087666f9c6a0a5bb3ad17993a7bff
Casper Rasmussen <cr@nodes.dk>
2015-04-28 21:25:06 +0200

    added NodesCommand which is a Command to run function in other classes - to avoid creating a lot of commands

---
+++ src/Commands/NodesCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NodesGearman;
+
+use App\Commands\Command;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+use Illuminate\Support\Facades\App;
+
+class NodesCommand extends Command implements SelfHandling, ShouldBeQueued {
+
+    use InteractsWithQueue, SerializesModels;
+
+    protected $data = [];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        \Log::info('Processing NodesCommand with data: ' . json_encode($this->data));
+
+        try {
+            $function = $this->data['action'];
+            $param = !empty($this->data['params']) ? $this->data['params'] : false;
+            $result = App::make($this->data['class'])->$function($param);
+        } catch(\Exception $e) {
+            return \Log::info('Error Processing NodesCommand with data: ' . json_encode($this->data) . ' result: ' . $e->getMessage());
+        }
+
+        \Log::info('Done Processing NodesCommand with data: ' . json_encode($this->data) . ' result: ' . json_encode($result));
+    }
+}
